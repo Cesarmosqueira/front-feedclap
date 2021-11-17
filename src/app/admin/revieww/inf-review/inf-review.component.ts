138b6156da6f958e1763259d0e18041d40575cb5
NataliaMelissa <u201816996@upc.edu.pe>
2021-11-17 00:14:53 -0500

    crear componente review vacio

---
+++ src/app/admin/revieww/inf-review/inf-review.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-inf-review',
+  templateUrl: './inf-review.component.html',
+  styleUrls: ['./inf-review.component.css']
+})
+export class InfReviewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/admin/revieww/revieww.model.ts
@@ -0,0 +1,7 @@
+export class Review {
+    id: number;
+    description: String;
+    rating: number;
+    status: String;
+    cost: number;
+}
